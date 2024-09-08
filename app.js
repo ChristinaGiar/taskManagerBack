4e165d0133f2f615f540b5a40d64787202f66839
Christina Giaramani <christinagiaramani@gmail.com>
2024-09-08 19:26:39 +0300

    Helmet added for extra security

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const dotenv = require('dotenv')
-
+const helmet = require('helmet')
 const authRoutes = require('./routes/auth')
 const dbRoutes = require('./routes/db')
 const { connection } = require('./utils/dbSetUp')
@@ -13,6 +13,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use(helmet())
 app.use(bodyParser.json())
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
